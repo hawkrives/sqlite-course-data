e391634b07a9abf69255aa4e74de65963c1373d5
Hawken Rives <rives@stolaf.edu>
2018-07-05 08:14:11 -0500

    auto-create data/ dir

--- download.js
+++ download.js
@@ -1,11 +1,13 @@
 const fs = require('fs')
 const got = require('got')
+const mkdir = require('make-dir')
 
 async function main() {
 	let {body: info} = await got.get('https://StoDevX.github.io/course-data/info.json', {json: true})
 
 	let files = info.files.filter(file => file.type === 'json').filter(file => file.year >= 2013)
 
+	await mkdir('./data')
 	let promises = files.map(file =>
 		got.get(`https://StoDevX.github.io/course-data/${file.path}`).then(data => {
 			let path = file.path.replace('terms', 'data')
